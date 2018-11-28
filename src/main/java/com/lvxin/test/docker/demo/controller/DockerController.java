8b15ced7db3b3bbea334fe0c206d763b9df19029
lvxin <562331245@qq.com>
2018-11-28 21:44:32 +0800

    docker

---
+++ src/main/java/com/lvxin/test/docker/demo/controller/DockerController.java
@@ -0,0 +1,13 @@
+package com.lvxin.test.docker.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DockerController {
+
+    @RequestMapping("/")
+    public String test(){
+        return "Hello Docker!!!";
+    }
+}
